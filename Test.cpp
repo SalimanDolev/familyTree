568ee54a0fff93264a3d3516d67fbd6bd204692f
SalimanDolev <44755169+SalimanDolev@users.noreply.github.com>
2020-04-04 16:57:33 +0300

    Add files via upload

---
+++ Test.cpp
@@ -0,0 +1,38 @@
+#include "doctest.h"
+#include "FamilyTree.hpp"
+#include <string>
+
+using namespace family;
+using namespace std;
+
+
+/////////////////////////////////////////////////////////////////////
+////////////////////////Tree checks//////////////////////////////////
+/////////////////////////////////////////////////////////////////////
+
+
+TEST_CASE("Test new tree build") {
+    
+}    
+
+TEST_CASE("Unknown members") {
+    
+}
+
+TEST_CASE("Test relationships") {
+    
+}
+
+TEST_CASE("Test multiple adds") {
+    
+}
+
+
+TEST_CASE("Test display") {
+    
+}
+
+
+TEST_CASE("Test find") {
+    
+}
